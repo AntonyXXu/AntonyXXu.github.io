95a36ec7d4a0c9153bdfe3e9d47dcae6db710c86
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-05 13:05:26 -0600

    components update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+#Personal Portfolio Website using React
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="header">
+        <div class="d-flex flex-column">
+          <div class="profile">
+            <img
+              src="media/profile.jpg"
+              alt=""
+              class="img-fluid rounded-circle"
+            ></img>
+            <h1 class="text-light">
+              <a href="/">Antony Xu</a>
+            </h1>
+            <div class="social-links mt-3 text-center">
+              <a
+                href="https://github.com/AntonyXXu"
+                target="_blank"
+                class="github"
+              >
+                <i class="bx bxl-skype"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/antonyxxu/"
+                target="_blank"
+                class="linkedin"
+              >
+                <i class="bi bi-linkedin"></i>
+              </a>
+              <a href="mailto:antonyxutong@gmail.com" class="email">
+                <i class="bx bxl-skype"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <nav class="nav-menu">
+          <ul>
+            <li class="active">
+              <a href="index.html">
+                <i class="bx bx-home"></i> <span>Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#about">
+                <i class="bx bx-user"></i> <span>About Me</span>
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio">
+                <i class="bx bx-book-content"></i> <span>Experience</span>
+              </a>
+            </li>
+            <li>
+              <a href="#resume">
+                <i class="bx bx-file-blank"></i> <span>Resume</span>
+              </a>
+            </li>
+            <li>
+              <a href="#contact">
+                <i class="bx bx-envelope"></i> Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button type="button" class="mobile-nav-toggle d-xl-none">
+          <i class="icofont-navigation-menu"></i>
+        </button>
       </header>
+      <section
+        id="hero"
+        class="d-flex flex-column justify-content-center align-items-center"
+      >
+        <div class="hero-container" data-aos="fade-in">
+          <h1>Antony Xu</h1>
+          <p>
+            I'm{" "}
+            <span
+              class="typed"
+              data-typed-items="Designer, Developer, Freelancer, Photographer"
+            ></span>
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
