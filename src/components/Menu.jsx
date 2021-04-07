e246b0cf3d3dd0f6e9f25718455556f6e10394ee
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-06 21:07:33 -0600

    folders updated

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import icons from "./logos/icons";
+import icons from "../assets/logos/icons";
 // import { ReactComponent as Linkedin } from "./logos/linkedin.svg";
 // import { ReactComponent as Github } from "./logos/github.svg";
 // import { ReactComponent as Email } from "./logos/email.svg";
